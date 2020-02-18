6666d06406ff3ff1a010e2320dc85d4f2b52cb69
snaipasomoh <myboloto99@gmail.com>
2020-02-18 10:30:03 +0300

    make_stat_row func added

--- l1/lab1.py
+++ l1/lab1.py
@@ -1,9 +1,21 @@
 import numpy as np
 
+def make_stat_row(seq):
+    seq.sort()
+    seq_no_dup = list(set(seq))
+    seq_no_dup.sort()
+    abs_freq = [seq.count(x) for x in seq_no_dup]
+    rel_freq = [x / len(seq) for x in abs_freq]
+    rel_freq_sums = [sum(rel_freq[0:i+1]) for i in range(len(rel_freq))]
+
+    # print(seq_no_dup)
+    # print(abs_freq)
+    # print(rel_freq)
+    # print(rel_freq_sums)
+
 with open("data.txt") as file:
     bin, geom, pois = [[int(x) for x in l.split()] for l in file]
 
-# print(bin, geom, pois, sep = "\n\n\n")
-bin.sort()
-geom.sort()
-pois.sort()
+#print(make_stat_row(bin))
+#print(make_stat_row(geom))
+print(make_stat_row(pois))
